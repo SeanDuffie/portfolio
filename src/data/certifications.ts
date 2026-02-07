4921b1a5413b2cb54d2f77b6953cd6ae498946d7
SeanDuffie <sduffie3@gatech.edu>
2026-02-07 15:29:52 -0500

    Update Certifications and Experience

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -11,30 +11,43 @@ export interface Certification {
 
 export const certifications: Certification[] = [
   {
-    id: 3,
+    id: 4,
     Year: 2026,
     Month: 6,
     issuer: "Amazon Web Services (AWS)",
     credentialName: "AWS Certified Solutions Architect",
     credentialId: "LEIDOS-12345",
-    description: "[TODO: Description for the AWS Certified Solutions Architect certification.]",
+    description:
+      "[TODO: Description for the AWS Certified Solutions Architect certification.]",
   },
   {
-    id: 2,
+    id: 3,
     Year: 2026,
     Month: 6,
     issuer: "Microsoft",
     credentialName: "Azure Solutions Architect Expert",
     credentialId: "MS-12345",
-    description: "[TODO: Description for the Azure Solutions Architect Expert certification.]",
+    description:
+      "[TODO: Description for the Azure Solutions Architect Expert certification.]",
   },
   {
-    id: 1,
+    id: 2,
     Year: 2024,
     Month: 9,
     issuer: "Google",
     credentialName: "Google Cloud Professional Data Engineer",
     credentialId: "GC-PDE-11223",
-    description: "[TODO: Description for the Google Cloud Professional Data Engineer certification.]",
+    description:
+      "[TODO: Description for the Google Cloud Professional Data Engineer certification.]",
+  },
+  {
+    id: 1,
+    Year: 2024,
+    Month: 9,
+    issuer: "OSHWLab",
+    credentialName: "EasyEDA Certified Professional",
+    credentialId: "OSHW-12345",
+    description:
+      "[TODO: Description for the EasyEDA Certified Professional certification.]",
   },
 ];
